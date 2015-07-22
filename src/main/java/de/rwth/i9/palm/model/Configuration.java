730aad8455bb6671c93bcd9f1555be6c81815779
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-07-22 08:49:02 +0200

    added configuration entity

---
+++ src/main/java/de/rwth/i9/palm/model/Configuration.java
@@ -0,0 +1,55 @@
+package de.rwth.i9.palm.model;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.MapKeyColumn;
+
+import de.rwth.i9.palm.persistence.PersistableResource;
+
+@Entity( name = "configuration" )
+public class Configuration extends PersistableResource
+{
+	@Column( unique = true )
+	String name;
+
+	@ElementCollection
+	@MapKeyColumn( name = "key" )
+	@Column( name = "value" )
+	@CollectionTable( name = "configuration_map" )
+	Map<String, String> configurationMap;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName( String name )
+	{
+		this.name = name;
+	}
+
+	public Map<String, String> getConfigurationMap()
+	{
+		return configurationMap;
+	}
+
+	public void setConfigurationMap( Map<String, String> configurationMap )
+	{
+		this.configurationMap = configurationMap;
+	}
+
+	public Configuration addConfigurationMap( String key, String value )
+	{
+		if ( configurationMap == null )
+			configurationMap = new LinkedHashMap<String, String>();
+
+		configurationMap.put( key, value );
+
+		return this;
+	}
+}
